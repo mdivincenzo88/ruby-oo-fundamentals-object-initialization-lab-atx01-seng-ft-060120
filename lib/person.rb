660bbe13da4382c960d618c5071fd884ab53f1c3
Martin Di Vincenzo <mdivincenz@hotmail.com>
2020-06-03 02:46:58 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,14 @@
 class Person
   def initialize(person_name)
+    @person_name = person_name
+  end
+  def person_name=(person_name)
+    @person_name = person_name
+  end
+  
+  def person_name
+    @person_name
+  end
+  
+end
     
