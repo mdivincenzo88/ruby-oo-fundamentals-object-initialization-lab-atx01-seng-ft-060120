66071fc38ef4af51c32ca84c8bc171319977f387
Martin Di Vincenzo <mdivincenz@hotmail.com>
2020-06-03 02:49:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,15 @@
 class Dog 
-
-
-
-
+  def initialize(name, breed="Mutt")
+    @name = name
+    @breed = breed 
+  end
+  def person_name=(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
 end
+    
